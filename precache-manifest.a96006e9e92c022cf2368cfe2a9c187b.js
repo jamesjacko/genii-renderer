3fd2b1296d0fcc163453d7ff5b64c8afe2fe8a40
James <james.ogge@gmail.com>
2019-11-26 02:25:53 +0000

    Updates

---
+++ precache-manifest.a96006e9e92c022cf2368cfe2a9c187b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9ad5e256bb41bbab31207e7c5582b7",
+    "url": "/murvRenderer/index.html"
+  },
+  {
+    "revision": "103d557cb4bc7c7de989",
+    "url": "/murvRenderer/static/css/main.73a58b1d.chunk.css"
+  },
+  {
+    "revision": "62f20dc0a85db884feef",
+    "url": "/murvRenderer/static/js/2.901d6a9f.chunk.js"
+  },
+  {
+    "revision": "103d557cb4bc7c7de989",
+    "url": "/murvRenderer/static/js/main.09ffefa3.chunk.js"
+  },
+  {
+    "revision": "f0e07b99de575adf8c3d",
+    "url": "/murvRenderer/static/js/runtime-main.bf15c1d9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/murvRenderer/static/media/logo.25bf045c.svg"
+  }
+]);
